f367399ff16a5bb3095fb48b745564bdfd2df77d
João Vitor Goncalves Pina <joaovitorpinag@hotmail.com>
2022-05-11 20:35:02 -0300

    Ajustado path do swagger
    Signed-off-by: João Vitor Goncalves Pina <joaovitorpinag@hotmail.com>

--- src/ioc/configurations/swagger.configuration.ts
+++ src/ioc/configurations/swagger.configuration.ts
@@ -10,6 +10,6 @@ export class SwaggerConfiguration {
       .build();
 
     const document = SwaggerModule.createDocument(app, config);
-    SwaggerModule.setup(`/api`, app, document);
+    SwaggerModule.setup(`/unidades/api`, app, document);
   }
 }
